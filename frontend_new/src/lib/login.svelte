f946ec5f805d40af076117ab364c01c5cf277d43
Chris Burrows <burrows.ca@gmail.com>
2024-06-26 13:00:44 +0100

    Basic login page skeleton

---
+++ frontend_new/src/lib/login.svelte
@@ -0,0 +1,14 @@
+<script>
+import Logo from "$lib/images/fastapi-logo.svg";
+
+let username = "";
+let password = "";
+</script>
+      
+<form class="h-screen max-w-sm mx-auto flex flex-col justify-center gap-4">
+    <img src={Logo} alt="FastAPI logo" class="self-center mb-4 h-auto max-w-xs" />
+    <input bind:value={username} placeholder="Email" type="email" required class="input input-bordered" />
+    <input bind:value={password} placeholder="Password" type='password' required class="input input-bordered" />
+    <a href="/recover-password" class="link link-primary self-center">Forgot password?</a>
+    <button type="submit" class="btn variant-filled-primary">Log In</button>
+</form>
--- frontend_new/src/routes/login/+page.svelte
+++ frontend_new/src/routes/login/+page.svelte
@@ -1,5 +1,11 @@
+<script>
+import Login from "$lib/login.svelte";
+</script>
+
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Login Page</h1>
+		<h1 class="h1">Login</h1>
 	</div>
 </div>
+
+<Login />
